625063105ad78d3ecf362f747d7410052753fab4
arap <arapmaklum999@gmail.com>
2022-12-15 02:10:39 +0700

    changes if statement into switch case

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 var score = prompt("masukkan nilai anda");
 
-if (score >=90){
-    console.log("Selamat! anda mendapat nilai A.");
-}else if (score >=80 || score <=89){
-    console.log("anda mendapat nilai B+.");
-}else if (score >=70 || score <=79){
-    console.log("anda mendapat nilai B-.");
-}else if (score >=60 || score <=69){
-    console.log("anda mendapat nilai C+.");
-}else{
-    console.log("maaf anda tidak lulus");
-}
+switch (score) {
+    case 90:
+        console.log("anda mendapat nilai A+");
+        break
+    case 80:
+        console.log("anda mendapat  nilai B+");
+        break
+    case 75:
+        console.log("anda mendapat nilai B-");
+        break
+    case 60:
+        console.log("maaf anda tidak lulu");
+        break
+};
